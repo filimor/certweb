d38c6b261fa00b7ceaeee692654d59601535186c
Filipe Moreira <filimor@posteo.com.br>
2019-12-31 21:06:16 -0300

    54. Design - Tela Links

---
+++ Certweb/Links.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Certweb
+{
+    partial class Links
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.txtLink = new System.Windows.Forms.TextBox();
+            this.lblLinks = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblDescricao = new System.Windows.Forms.Label();
+            this.llbExcluir = new System.Windows.Forms.LinkLabel();
+            this.llbEditar = new System.Windows.Forms.LinkLabel();
+            this.lblLink = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.btnSalvar.FlatAppearance.BorderSize = 0;
+            this.btnSalvar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalvar.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalvar.ForeColor = System.Drawing.Color.White;
+            this.btnSalvar.Location = new System.Drawing.Point(17, 94);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(88, 33);
+            this.btnSalvar.TabIndex = 0;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = false;
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDescricao.Location = new System.Drawing.Point(17, 41);
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(300, 37);
+            this.txtDescricao.TabIndex = 1;
+            this.txtDescricao.Text = "Descrição";
+            // 
+            // txtLink
+            // 
+            this.txtLink.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLink.Location = new System.Drawing.Point(323, 41);
+            this.txtLink.Name = "txtLink";
+            this.txtLink.Size = new System.Drawing.Size(392, 37);
+            this.txtLink.TabIndex = 2;
+            this.txtLink.Text = "Link";
+            // 
+            // lblLinks
+            // 
+            this.lblLinks.AutoSize = true;
+            this.lblLinks.Font = new System.Drawing.Font("Century Gothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLinks.Location = new System.Drawing.Point(3, 134);
+            this.lblLinks.Name = "lblLinks";
+            this.lblLinks.Size = new System.Drawing.Size(173, 78);
+            this.lblLinks.TabIndex = 3;
+            this.lblLinks.Text = "Links";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.panel1);
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(17, 212);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(698, 252);
+            this.flowLayoutPanel1.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblLink);
+            this.panel1.Controls.Add(this.llbEditar);
+            this.panel1.Controls.Add(this.llbExcluir);
+            this.panel1.Controls.Add(this.lblDescricao);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(695, 20);
+            this.panel1.TabIndex = 5;
+            // 
+            // lblDescricao
+            // 
+            this.lblDescricao.AutoSize = true;
+            this.lblDescricao.Dock = System.Windows.Forms.DockStyle.Left;
+            this.lblDescricao.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescricao.Location = new System.Drawing.Point(0, 0);
+            this.lblDescricao.Name = "lblDescricao";
+            this.lblDescricao.Size = new System.Drawing.Size(140, 16);
+            this.lblDescricao.TabIndex = 0;
+            this.lblDescricao.Text = "Curso de Windows Forms";
+            // 
+            // llbExcluir
+            // 
+            this.llbExcluir.AutoSize = true;
+            this.llbExcluir.Dock = System.Windows.Forms.DockStyle.Right;
+            this.llbExcluir.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.llbExcluir.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.llbExcluir.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.llbExcluir.Location = new System.Drawing.Point(635, 0);
+            this.llbExcluir.Name = "llbExcluir";
+            this.llbExcluir.Size = new System.Drawing.Size(60, 16);
+            this.llbExcluir.TabIndex = 1;
+            this.llbExcluir.TabStop = true;
+            this.llbExcluir.Text = "linkLabel1";
+            // 
+            // llbEditar
+            // 
+            this.llbEditar.AutoSize = true;
+            this.llbEditar.Dock = System.Windows.Forms.DockStyle.Right;
+            this.llbEditar.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.llbEditar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.llbEditar.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.llbEditar.Location = new System.Drawing.Point(575, 0);
+            this.llbEditar.Name = "llbEditar";
+            this.llbEditar.Size = new System.Drawing.Size(60, 16);
+            this.llbEditar.TabIndex = 2;
+            this.llbEditar.TabStop = true;
+            this.llbEditar.Text = "linkLabel2";
+            // 
+            // lblLink
+            // 
+            this.lblLink.AutoSize = true;
+            this.lblLink.Dock = System.Windows.Forms.DockStyle.Left;
+            this.lblLink.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.lblLink.Location = new System.Drawing.Point(140, 0);
+            this.lblLink.Name = "lblLink";
+            this.lblLink.Size = new System.Drawing.Size(204, 13);
+            this.lblLink.TabIndex = 3;
+            this.lblLink.Text = "http://www.cursodewindowsforms.com.br";
+            // 
+            // Links
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.lblLinks);
+            this.Controls.Add(this.txtLink);
+            this.Controls.Add(this.txtDescricao);
+            this.Controls.Add(this.btnSalvar);
+            this.Name = "Links";
+            this.Size = new System.Drawing.Size(735, 477);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.TextBox txtLink;
+        private System.Windows.Forms.Label lblLinks;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblLink;
+        private System.Windows.Forms.LinkLabel llbEditar;
+        private System.Windows.Forms.LinkLabel llbExcluir;
+        private System.Windows.Forms.Label lblDescricao;
+    }
+}
